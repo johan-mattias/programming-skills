8dbb4ec18fbe518a043a3d566aa5ec4a6635344c
Johan <3921204+johan-mattias@users.noreply.github.com>
2020-11-04 13:13:11 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ print(count)
 
 points = 7
 player_name = "lasse"
-player_number = 11
-if(player_number > 10):
+player = 11
+if(player > 10):
     if(player_name == "lasse"):
         points = points + 3
     else:
@@ -41,10 +41,10 @@ print(points)
 class_1 = "art"
 class_2 = "english"
 if(class_1 == "art" and class_2 == "biology"):
-    year = 2
+    year = "two"
 elif(class_1 == "art" or class_2 == "english"):
-    year = 3
-print(class_2 + year)
+    year = "three"
+print(class_2 + "_" + year)
 
 x_value = 23
 y_value = 3
@@ -69,12 +69,90 @@ elif(center == True):
         print("player one win column")
     else:
         print("player two win column")
-
-
+        
 a_value = 6
 b_value = 8 
-c_value = (a_value*a_value + b_value*b_value)/10
+c_value = (a_value*a_value + b_value*b_value)**(1/2)
 print(c_value)
+
+
+first = 21
+second = 8
+third = 13
+if(first < second and first > third):
+    print(first)
+elif(second < first and second > third):
+    print(second)
+elif(third < first and third > second):
+    print(third)
+
+
+value = 0
+for count in range(5):
+    value = value + count
+print(value)
+
+#program with loop and variable
+
+x_position = 4
+y_position = 7
+
+for value in range(4):
+    x_position = x_position + value
+    y_position = y_position + value
+
+print ("x: " + x_position + ", y: " + y_position)
+
+
+#program with list and loop (smallest value)
+
+number_list = [21, 8, 53, 13, 35]
+result = 100
+for value in number_list:
+    if value < result:
+        result = value
+print(result)
+
+
+#program with loop and string
+
+color_list = ["red", "white", "blue", "green"]
+color_in_string = ""
+
+for color in color_list:
+    color_in_string = color + ", " + color_in_string
+print(color_in_string)
+
+#program with stoplight
+
+first_light = "green"
+second_light = "yellow"
+third_light = "off"
+
+if(third_light == "red"):
+    third_light = "off"
+    second_light = "off"
+    first_light = "green"
+elif(second_light == "yellow" and first_light == "off"):
+    third_light = "red"
+    second_light = "off"
+    first_light = "off"
+elif(first_light == "green"):
+    third_light = "red"
+    second_light = "yellow"
+    first_light = "off"
+print(first_light + ", " + second_light + ", " + third_light)
+    
+    
+
+#bonus abstract
+
+
+
+
+
+
+
 
 
 
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let year: number;
+let year: string;
 console.log(11)
 let y_value = 11
 console.log(y_value)
@@ -25,8 +25,8 @@ if (timer != 3) {
 console.log(count)
 let points = 7
 let player_name = "lasse"
-let player_number = 11
-if (player_number > 10) {
+let player = 11
+if (player > 10) {
     if (player_name == "lasse") {
         points = points + 3
     } else {
@@ -41,12 +41,12 @@ console.log(points)
 let class_1 = "art"
 let class_2 = "english"
 if (class_1 == "art" && class_2 == "biology") {
-    year = 2
+    year = "two"
 } else if (class_1 == "art" || class_2 == "english") {
-    year = 3
+    year = "three"
 }
 
-console.log(class_2 + year)
+console.log(class_2 + "_" + year)
 let x_value = 23
 y_value = 3
 let z_value = 43
@@ -79,5 +79,65 @@ if (center == false) {
 
 a_value = 6
 b_value = 8
-c_value = (a_value * a_value + b_value * b_value) / 10
+c_value = (a_value * a_value + b_value * b_value) ** (1 / 2)
 console.log(c_value)
+let first = 21
+let second = 8
+let third = 13
+if (first < second && first > third) {
+    console.log(first)
+} else if (second < first && second > third) {
+    console.log(second)
+} else if (third < first && third > second) {
+    console.log(third)
+}
+
+let value = 0
+for (count = 0; count < 5; count++) {
+    value = value + count
+}
+console.log(value)
+// program with loop and variable
+let x_position = 4
+let y_position = 7
+for (value = 0; value < 4; value++) {
+    x_position = x_position + value
+    y_position = y_position + value
+}
+console.log("x: " + x_position + ", y: " + y_position)
+// program with list and loop (smallest value)
+let number_list = [21, 8, 53, 13, 35]
+let result = 100
+for (value of number_list) {
+    if (value < result) {
+        result = value
+    }
+    
+}
+console.log(result)
+// program with loop and string
+let color_list = ["red", "white", "blue", "green"]
+let color_in_string = ""
+for (let color of color_list) {
+    color_in_string = color + ", " + color_in_string
+}
+console.log(color_in_string)
+// program with stoplight
+let first_light = "green"
+let second_light = "yellow"
+let third_light = "off"
+if (third_light == "red") {
+    third_light = "off"
+    second_light = "off"
+    first_light = "green"
+} else if (second_light == "yellow" && first_light == "off") {
+    third_light = "red"
+    second_light = "off"
+    first_light = "off"
+} else if (first_light == "green") {
+    third_light = "red"
+    second_light = "yellow"
+    first_light = "off"
+}
+
+console.log(first_light + ", " + second_light + ", " + third_light)
